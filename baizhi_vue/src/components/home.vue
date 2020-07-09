7255d61b586431aca36b22a699e15f61e639bf40
myforgit <1404197345@qq.com>
2020-07-09 09:19:05 +0800

    百知商城

---
+++ baizhi_vue/src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header></Header>
+        <Banner></Banner>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+
+    import Header from "./commen/Header";
+    import Banner from "./commen/Banner";
+    import Footer from "./commen/Footer";
+
+    export default {
+        name: "Home",
+        components: {
+            Header: Header,
+            Banner: Banner,
+            Footer: Footer,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
